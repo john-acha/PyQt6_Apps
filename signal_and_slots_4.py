5a89b3c15761ca0963bc96332b87c8c802f0c119
John  Acha <120533881+john-acha@users.noreply.github.com>
2023-10-14 17:14:56 -0500

    Add files via upload
    Signals and Slots

---
+++ signal_and_slots_4.py
@@ -0,0 +1,35 @@
+from PyQt6.QtWidgets import (
+	QApplication, 
+	QMainWindow, 
+	QPushButton,
+)
+
+import sys
+
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+
+		self.button_is_checked = True
+
+		self.setWindowTitle("My App")
+
+		self.button = QPushButton("Press Me!")
+		self.button.setCheckable(True)
+		self.button.released.connect(self.the_button_was_released)
+		self.button.setChecked(self.button_is_checked)
+
+		# Set the central widget of the window.
+		self.setCentralWidget(self.button)
+
+	def the_button_was_released(self):
+		self.button_is_checked = self.button.isChecked() # returns the check state of the button
+
+		print(self.button_is_checked)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
+
